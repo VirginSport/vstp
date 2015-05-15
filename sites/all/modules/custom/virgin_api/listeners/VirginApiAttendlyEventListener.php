f58aa18dc885a250b4ce3ee2595dd5492c452172
Diogo Correia <inlogic@gmail.com>
2015-05-15 15:48:55 +0100

    Create an "Event State" content-type. Sync Attendly information to it instead of the "Event" content-type.
    An "Event" content-type is created automatically in the "Event State" creation procedure.

--- sites/all/modules/custom/virgin_api/listeners/VirginApiAttendlyEventListener.php
+++ sites/all/modules/custom/virgin_api/listeners/VirginApiAttendlyEventListener.php
@@ -51,7 +51,7 @@ class VirginApiAttendlyEventListener implements ObserverObserverInterface {
   }
 
   /**
-   * Transforms an event webhook data to a node.
+   * Transforms an event webhook data to a event state node.
    *
    * @param $data
    *  The webhook transformed data.
@@ -66,51 +66,77 @@ class VirginApiAttendlyEventListener implements ObserverObserverInterface {
     $event = $data->EventData->Event;
     $tickets = $data->EventData->Tickets;
 
-    // Check if the event already exists locally.
-    $node = virgin_api_get_entity_by_attendly_id('node', 'event', $event->ID);
+    // Check if the event state already exists locally.
+    $state = virgin_api_get_entity_by_attendly_id('node', 'event_state', $event->ID);
 
-    // If it doesn't, create a new event.
-    if (empty($node)) {
-      $node = new stdClass();
-      $node->type = 'event';
+    // If it doesn't, create a new event state.
+    if (empty($state)) {
+      $state = new stdClass();
+      $state->type = 'event_state';
     }
 
     // Ensure the node is ready for changes.
-    node_object_prepare($node);
+    node_object_prepare($state);
 
     // Fill in the information.
-    $node_wrapper = entity_metadata_wrapper('node', $node);
-    $node_wrapper->title->set($event->Name);
-    $node_wrapper->language->set($language->language);
-    $node_wrapper->field_event_id->set($event->ID);
-    $node_wrapper->field_event_parent_id->set($event->GroupID);
-    $node_wrapper->field_event_start_date->set(strtotime($event->Start));
-    $node_wrapper->field_event_end_date->set(strtotime($event->Stop));
-    $node_wrapper->field_event_reg_end_date->set(strtotime($event->Deadline));
-    $node_wrapper->field_twitter_hashtag->set($event->Hashtag);
-    $node_wrapper->body->set(array('value' => $event->Description, 'format' => 'full_html'));
+    $state_wrapper = entity_metadata_wrapper('node', $state);
+    $state_wrapper->title->set($event->Name);
+    $state_wrapper->language->set($language->language);
+    $state_wrapper->field_event_id->set($event->ID);
+    $state_wrapper->field_event_parent_id->set($event->GroupID);
+    $state_wrapper->field_event_start_date->set(strtotime($event->Start));
+    $state_wrapper->field_event_end_date->set(strtotime($event->Stop));
+    $state_wrapper->field_event_reg_end_date->set(strtotime($event->Deadline));
+    $state_wrapper->field_twitter_hashtag->set($event->Hashtag);
+    $state_wrapper->field_link->set(array('url' => $event->URL));
+    $state_wrapper->body->set(array('value' => $event->Description, 'format' => 'full_html'));
 
     // Purge all previously set ticket types.
-    $node_wrapper->field_ticket_types->set(array());
+    $state_wrapper->field_ticket_types->set(array());
 
     // Re-set the ticket types from the passed data.
     foreach ($tickets as $ticket) {
-      $collection = entity_create('field_collection_item', array('field_name' => 'field_ticket_types'));
-      $collection->setHostEntity('node', $node);
+      $ticket_type = entity_create('field_collection_item', array('field_name' => 'field_ticket_types'));
+      $ticket_type->setHostEntity('node', $state);
 
-      $collection_wrapper = entity_metadata_wrapper('field_collection_item', $collection);
-      $collection_wrapper->field_title->set($ticket->Name);
-      $collection_wrapper->field_short_description->set($ticket->Description);
+      $ticket_type_wrapper = entity_metadata_wrapper('field_collection_item', $ticket_type);
+      $ticket_type_wrapper->field_title->set($ticket->Name);
+      $ticket_type_wrapper->field_short_description->set($ticket->Description);
+      $ticket_type_wrapper->field_ticket_id->set($ticket->ID);
+      $ticket_type_wrapper->field_earlybird->set($ticket->Earlybird);
+      $ticket_type_wrapper->field_earlybird_date->set(strtotime($ticket->EarlybirdDate));
+      $ticket_type_wrapper->field_type->set($ticket->Type);
+      $ticket_type_wrapper->field_mode->set($ticket->Mode);
 
-      // Store the ticket value as a formatted currency string.
+      // Store value field with as formatted currency strings.
       $formatter = new NumberFormatter(locale_get_default(), NumberFormatter::CURRENCY);
+
       $value = $formatter->formatCurrency($ticket->Price, $event->Currency);
-      $collection_wrapper->field_value->set($value);
+      $ticket_type_wrapper->field_value->set($value);
+
+      $earlybird_value = $formatter->formatCurrency($ticket->Price, $event->Currency);
+      $ticket_type_wrapper->field_earlybird_value->set($earlybird_value);
     }
 
-    // Set the log message.
-    $node_wrapper->log->set('Attendly Webhook Synchronization');
+    // Check if an host event exists, if it doesn't create it now.
+    $event = $state_wrapper->field_event->value();
 
-    return $node;
+    if (empty($event)) {
+      $event = new stdClass();
+      $event->type = 'event';
+
+      node_object_prepare($event);
+
+      $event_wrapper = entity_metadata_wrapper('node', $event);
+      $event_wrapper->title->set($state_wrapper->title->value());
+      $event_wrapper->language->set($language->language);
+
+      node_save($event);
+
+      // Set the newly created event as the host of this event state.
+      $state_wrapper->field_event->set($event);
+    }
+
+    return $state;
   }
 }
