be3b867616ffa88139bf9abb5cf001e64ac0db38
Diogo Correia <inlogic@gmail.com>
2015-06-17 17:23:43 +0100

    Make the main menu and footer available in the page template.

--- docroot/sites/all/themes/custom/virginsports/template.php
+++ docroot/sites/all/themes/custom/virginsports/template.php
@@ -4,3 +4,45 @@
  * @file
  * Main file for the virginsports theme.
  */
+
+
+/**
+ * Implements hook_preprocess_HOOK() for page theme.
+ */
+function virginsports_preprocess_page(&$vars) {
+
+  // Menus
+  $vars['main_menu'] = virginsports_get_menu_tree('main-menu', 1);
+  $vars['footer_menu'] = virginsports_get_menu_tree('menu-footer-menu', 2);
+
+  unset($vars['main_menu']['#theme_wrappers']);
+  unset($vars['footer_menu']['#theme_wrappers']);
+}
+
+// Helpers
+// -----------------------------------------------------------------------------
+
+/**
+ * Gets a menu tree based on the current path with a customizable depth option.
+ *
+ * @param $menu_name
+ *   The name of the menu.
+ * @param $max_depth
+ *   The max depth of links to retrieve.
+ * @return
+ *   A structured array representing the specified menu on the current page, to
+ *   be rendered by drupal_render().
+ */
+function virginsports_get_menu_tree($menu_name, $max_depth = NULL) {
+  $menu_output = &drupal_static(__FUNCTION__, array());
+  $index = empty($max_depth) ? 0 : $max_depth;
+  $menu_key = $menu_name + '-' + $index;
+
+  if (!isset($menu_output[$menu_name])) {
+    $tree = menu_tree_page_data($menu_name, $max_depth);
+
+    $menu_output[$menu_key] = menu_tree_output($tree);
+  }
+
+  return $menu_output[$menu_key];
+}
