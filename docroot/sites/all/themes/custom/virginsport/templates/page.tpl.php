c510400bac6d1eb138297eccd7fa905cf8c587ff
Diogo Correia <inlogic@gmail.com>
2015-06-26 12:18:11 +0100

    Conditionally show tabs and title in the content section.

--- docroot/sites/all/themes/custom/virginsport/templates/page.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/page.tpl.php
@@ -137,23 +137,27 @@
 <!-- /#main-menu -->
 
 <section id="content">
-  <?php if (!$page_manager): ?><div class="container"><?php endif; ?>
-  <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-  <a id="main-content"></a>
-  <?php print render($title_prefix); ?>
-  <?php if (!empty($title)): ?>
-    <h1 class="page-header"><?php print $title; ?></h1>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php print $messages; ?>
-  <?php if (!empty($tabs)): ?>
-    <?php print render($tabs); ?>
-  <?php endif; ?>
-  <?php if (!empty($action_links)): ?>
-    <ul class="action-links"><?php print render($action_links); ?></ul>
+  <?php if (!$page_manager): ?>
+    <div class="container">
+    <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+    <a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+      <h1 class="page-header"><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php print $messages; ?>
+    <?php if (!empty($tabs)): ?>
+      <?php print render($tabs); ?>
+    <?php endif; ?>
+    <?php if (!empty($action_links)): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
   <?php endif; ?>
   <?php print render($page['content']); ?>
-  <?php if (!$page_manager): ?></div><?php endif; ?>
+  <?php if (!$page_manager): ?>
+    </div>
+  <?php endif; ?>
 </section>
 <!-- /#content -->
 
