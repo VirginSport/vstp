b1be4f58d5f1a81199001f3aaf06311c8b5be9aa
Graham Arrowsmith <graham.arrowsmith@acquia.com>
2016-10-03 18:34:11 +0100

    add database backup and refactor to avoid hardcoding site parameters

--- hooks/common/post-code-deploy/clear-caches.sh
+++ hooks/common/post-code-deploy/clear-caches.sh
@@ -6,18 +6,40 @@ repo_url="$5"
 repo_type="$6"
 
 drush_alias=$site'.'$target_env
-domain="${site}${target_env}.prod.acquia-sites.com"
 
 if [ "$target_env" != 'prod' ]; then
-    echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
-    echo "Updating database."
-    drush @$drush_alias updb -y
-    echo "Running feature revert."
-    drush @$drush_alias fra -y
-    echo "Clearing Drupal cache."
-    drush @$drush_alias cc all
-    echo "Clearing Varnish cache."
-    drush @$drush_alias ac-domain-purge $domain
+  echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
+  #
+  # Backs up all databases in the current environment
+  #
+  echo "Backing up database(s)..."
+  for db in $(drush @$drush_alias ac-database-list | awk '{print $3}'); do
+    echo "Backing up ${db}..."
+    drush @$drush_alias ac-database-instance-backup $db
+  done
+  #
+  # Update database and feature revert
+  #
+  echo "Updating database."
+  drush @$drush_alias updb -y
+  echo "Running feature revert."
+  drush @$drush_alias fra -y
+  #
+  # Clear Drupal cache
+  #
+  echo "Clearing Drupal cache."
+  drush @$drush_alias cc all
+  #
+  # Clear Varnish cache for all domains on this environment
+  #
+  for domain in $(drush @$drush_alias ac-domain-list | awk '{print $3}'); do
+    if [[ $domain == *"amazonaws"* ]]; then
+      echo "This is an ELB, skipping varnish cache clear for ${domain}..."
+    else
+      echo "Clearing Varnish cache for ${domain}..."
+      drush @$drush_alias ac-domain-purge $domain
+    fi
+  done
 else
-    echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
+  echo "$site.$target_env: The $source_branch branch has been updated on $target_env."
 fi
