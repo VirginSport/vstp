8feb216726ee434d7369bfd4b2cc273b1a7bb319
GomeZ <joaogomes_cd@hotmail.com>
2016-10-03 17:21:39 +0100

    Add script to redirect user to region hostname defined in cookie if it exists

---
+++ docroot/sites/all/modules/custom/virgin_region/js/virgin_region_page_splash_redirect.js
@@ -0,0 +1,39 @@
+(function () {
+    /**
+     *  Get a cookie value
+     *
+     * @param name
+     *  The name of the cookie
+     * @returns string|null
+     *  The cookie value or null if not found
+     */
+    function getCookie(name) {
+        var encodedName = encodeURIComponent(name) + "=";
+
+        // Split cookie list string into an array, by default cookies are
+        // separated by a ; and white space
+        var cookieList = document.cookie.split('; ');
+
+        for (var i = 0; i < cookieList.length; i++) {
+            var cookie = cookieList[i];
+
+            // If encoded name exists return the value portion of cookie, that comes
+            // after the encoded name
+            if (cookie.indexOf(encodedName) === 0) {
+                var cookieValue = cookie.substring(encodedName.length, cookie.length);
+                return decodeURIComponent(cookieValue);
+            }
+        }
+
+        // Default cookie value
+        return null;
+    }
+
+    var regionHostname = getCookie('virgin_sport_region_hostname');
+
+    if (regionHostname && location.hostname != regionHostname) {
+        var protocol = location.protocol;
+
+        location.href = protocol + '//' + regionHostname;
+    }
+}());
