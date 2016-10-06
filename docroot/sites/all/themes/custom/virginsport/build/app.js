f8c26142b1a4a569d0841041e80d59032133864d
GomeZ <joaogomes_cd@hotmail.com>
2016-10-06 11:10:44 +0100

    Frontend sync

--- docroot/sites/all/themes/custom/virginsport/build/app.js
+++ docroot/sites/all/themes/custom/virginsport/build/app.js
@@ -50,6 +50,11 @@
 	__webpack_require__(1);
 	__webpack_require__(2);
 
+	// Build images into build folder, requireAll is not available on compiled js file
+	if (typeof requireAll === 'function') {
+	    requireAll(__webpack_require__(3));
+	}
+
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
@@ -63,6 +68,181 @@
 	'use strict';
 
 	console.log('Virgin Sport');
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var map = {
+		"./banner-example.png": 4,
+		"./basket.png": 5,
+		"./basket.svg": 6,
+		"./content-block-example.png": 7,
+		"./hero-event-card-example.png": 8,
+		"./hero-event-card-example@2x.png": 9,
+		"./splash-page-pattern.png": 10,
+		"./vs-footer-uk-flag.png": 11,
+		"./vs-footer-uk-flag.svg": 12,
+		"./vs-logo-alternate.png": 13,
+		"./vs-logo-alternate.svg": 14,
+		"./vs-logo-red@2x.png": 16,
+		"./vs-logo-small.png": 17,
+		"./vs-logo-small.svg": 18,
+		"./vs-logo.png": 19,
+		"./vs-logo.svg": 20,
+		"./vs-region-select-uk.png": 21,
+		"./vs-region-select-uk@2x.png": 22,
+		"./vs-region-select-us.png": 23,
+		"./vs-region-select-us@2x.png": 24,
+		"./vs-uk-flag.svg": 25,
+		"./vs-us-flag.svg": 26
+	};
+	function webpackContext(req) {
+		return __webpack_require__(webpackContextResolve(req));
+	};
+	function webpackContextResolve(req) {
+		return map[req] || (function() { throw new Error("Cannot find module '" + req + "'.") }());
+	};
+	webpackContext.keys = function webpackContextKeys() {
+		return Object.keys(map);
+	};
+	webpackContext.resolve = webpackContextResolve;
+	module.exports = webpackContext;
+	webpackContext.id = 3;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/banner-example.png";
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/basket.png";
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/basket.svg";
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/content-block-example.png";
+
+/***/ },
+/* 8 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/hero-event-card-example.png";
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/hero-event-card-example@2x.png";
+
+/***/ },
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/splash-page-pattern.png";
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-footer-uk-flag.png";
+
+/***/ },
+/* 12 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-footer-uk-flag.svg";
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo-alternate.png";
+
+/***/ },
+/* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo-alternate.svg";
+
+/***/ },
+/* 15 */,
+/* 16 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo-red@2x.png";
+
+/***/ },
+/* 17 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo-small.png";
+
+/***/ },
+/* 18 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo-small.svg";
+
+/***/ },
+/* 19 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo.png";
+
+/***/ },
+/* 20 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-logo.svg";
+
+/***/ },
+/* 21 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-region-select-uk.png";
+
+/***/ },
+/* 22 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-region-select-uk@2x.png";
+
+/***/ },
+/* 23 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-region-select-us.png";
+
+/***/ },
+/* 24 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-region-select-us@2x.png";
+
+/***/ },
+/* 25 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-uk-flag.svg";
+
+/***/ },
+/* 26 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "public/img/vs-us-flag.svg";
 
 /***/ }
 /******/ ]);
--- docroot/sites/all/themes/custom/virginsport/webpack.entry.js
+++ docroot/sites/all/themes/custom/virginsport/webpack.entry.js
@@ -1,3 +1,8 @@
 // Webpack JS entry file
-require('./src/scss/main.scss')
-require('./src/js/main.js')
+require('./src/scss/main.scss');
+require('./src/js/main.js');
+
+// Build images into build folder, requireAll is not available on compiled js file
+if (typeof requireAll === 'function') {
+    requireAll(require.context('./src/img/', true, /.(png|jpg|jpeg|svg)$/));
+}
