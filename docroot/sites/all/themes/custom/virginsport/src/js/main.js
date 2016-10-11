a8b50e8a100d9627d0be5191b1bed06892b31381
Diogo Correia <inlogic@gmail.com>
2016-10-11 17:40:34 +0100

    Temporarily apply round corners via the main.js file.

--- docroot/sites/all/themes/custom/virginsport/src/js/main.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/main.js
@@ -1,2 +1,20 @@
+import RoundCorner from './components/round-corner';
+import withClass from './helper/with-class';
+import onResize from './helper/on-resize';
 
-console.log('Virgin Sport');
+// Apply the rounded corners to all vs-region--rounded
+let rounded = [];
+
+withClass('vs-region--rounded', el => {
+  rounded.push(new RoundCorner(el))
+});
+
+let update = () => {
+  rounded.forEach(round  => round.update());
+};
+
+// Re-calculate the size of the rounded elements whenever the window is resized
+onResize(update);
+
+// Check each 500ms if the containers change size and need to be updated
+window.setInterval(update, 500);
