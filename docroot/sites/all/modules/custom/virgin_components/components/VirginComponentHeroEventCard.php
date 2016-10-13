b25c3ce4a0a97e23f84162f3b63ad2fa4022cbbc
Luís Freitas <luis.freitas@driven.pt>
2016-10-13 11:41:27 +0100

    Hero event card default template updated

--- docroot/sites/all/modules/custom/virgin_components/components/VirginComponentHeroEventCard.php
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentHeroEventCard.php
@@ -14,15 +14,14 @@ class VirginComponentHeroEventCard implements VirginComponentsInterface {
    * {@inheritdoc}
    */
   public function supports($entity_type, $bundle) {
-    return FALSE;
+      return $entity_type == 'fieldable_panels_pane' && $bundle == 'vs_hero_event_card';
   }
 
   /**
    * {@inheritdoc}
    */
   public function themeSuggestion() {
-    // TODO
-    return '';
+      return 'virgin_components__fpp__vs_hero_event_card';
   }
 
   /**
