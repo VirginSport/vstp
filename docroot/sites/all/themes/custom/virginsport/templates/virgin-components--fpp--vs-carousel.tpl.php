6d5b2201480e0891ac5823fd45d33a19a11cccf3
Luís Freitas <luis.freitas@driven.pt>
2016-10-13 11:27:14 +0100

    Add Carousel default template

---
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-carousel.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file virgin-components--fpp--vs-carousel.tpl.php
+ * Main template for virgin components fieldable panel panes vs_carousel
+ *
+ * Variables available:
+ * TODO
+ */
+?>
+
+Carousel Template
