d94f10059667e251e8d25b8ed3620ae6db5639af
GomeZ <joao.gomes@driven.pt>
2016-10-13 14:51:11 +0100

    Add Paragraph custom banner

---
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentCustomBanner.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains VirginComponentCustomBanner class
+ */
+
+/**
+ * Class VirginComponentCustomBanner
+ */
+class VirginComponentCustomBanner implements VirginComponentsInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supports($entity_type, $bundle) {
+      return $entity_type == 'paragraphs_item' && $bundle == 'vs_custom_banner';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function themeSuggestion() {
+      return 'virgin_components__p__vs_custom_banner';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preProcess(&$variables) {
+    // TODO
+  }
+}
