ab16a9fcf55930417ebdf5c0a2f70c21014421cb
Luís Freitas <luis.freitas@driven.pt>
2016-10-17 17:33:12 +0100

    Add hero banner block component

--- docroot/sites/all/modules/custom/virgin_components/components/VirginComponentHeroBannerBlock.php
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentHeroBannerBlock.php
@@ -28,6 +28,16 @@ class VirginComponentHeroBannerBlock implements VirginComponentsInterface {
    * {@inheritdoc}
    */
   public function preProcess(&$variables) {
-    // TODO
+      if (empty($variables['elements']['#fieldable_panels_pane'])) {
+          return;
+      }
+
+      $variables['fpp'] = $variables['elements']['#fieldable_panels_pane'];
+      $virgin = new VirginEntityGrapher('fieldable_panels_pane', $variables['fpp']);
+
+      $variables['hero_banner_title'] = $virgin->fieldGetOne('title_field');
+      $variables['hero_banner_subheading'] = $virgin->fieldGetOne('field_description');
+      $variables['hero_banner_image'] = $virgin->relation('field_hero_image');
+      $variables['cta_links'] = $virgin->fieldGetAll('field_cta_links');
   }
 }
