6c927a0eeee3db739d3ca32b59b8ff260b49702f
Luís Freitas <luis.freitas@driven.pt>
2016-10-20 15:49:37 +0100

    Remove the standard or vip information about ticket

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--p--vs-ticket-card.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--p--vs-ticket-card.tpl.php
@@ -33,36 +33,10 @@
           <?php if(!empty($price)): ?>
           <span class="vs-ticket-type__price"><?php print virginsport_currency($currency, $price); ?></span>
           <?php endif; ?>
-          <button class="btn vs-btn vs-btn--sm vs-btn--transparent vs-ticket-type__cta">Select tickets</button>
-          <div class="vs-ticket-type__pass hidden-sm-down">
-            <span>Standard Pass</span>
-          </div>
+          <?php print l(t('Select tickets'), 'node/'.$nid, array('attributes' => array('class' => array('btn vs-btn vs-btn--sm vs-btn--transparent vs-ticket-type__cta')))); ?>
         </div>
       </div>
     </div>
   </div>
 </div>
 
-
-<!--<div class="vs-ticket-type">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="vs-ticket-type__wrapper materialShadow vs-ticket-type--gradient-blue">
-          <h2 class="vs-ticket-type__title vs-ticket-type__title--vip">
-            5K Run
-          </h2>
-          <p class="vs-ticket-type__text vs-ticket-type__text--vip">
-            Nunc laoreet ullamcorper tellus, ac lacinia metus mattis a. Praesent ut arcu ac tortor ornare mollis et ac justo. Sed eleifend eu dolor ac blandit.
-          </p>
-
-          <span class="vs-ticket-type__price">£35</span>
-          <button class="btn vs-btn vs-btn--sm vs-btn--transparent vs-ticket-type__cta">Select tickets</button>
-          <div class="vs-ticket-type__vip">
-            <span>Vip Pass</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>-->
