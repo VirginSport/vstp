eb6f7db6aa96cc1de257fddb84d8144bc38f7b0c
Luís Freitas <luis.freitas@driven.pt>
2016-10-20 15:20:12 +0100

    Add Ticket Card variables and defined template

--- docroot/sites/all/modules/custom/virgin_components/components/VirginComponentTicketCardItem.php
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentTicketCardItem.php
@@ -28,6 +28,20 @@ class VirginComponentTicketCardItem implements VirginComponentsInterface {
    * {@inheritdoc}
    */
   public function preProcess(&$variables) {
-    // TODO
+    if (empty($variables['elements']['#entity'])) {
+      return;
+    }
+
+    $variables['p'] = $variables['elements']['#entity'];
+    $virgin = new VirginEntityGrapher('paragraphs_item', $variables['p']);
+
+    $grapher = $virgin->relation('field_ticket_reference');
+    $variables['title'] = $grapher->fieldGetOne('title_field');
+    $variables['description'] = $grapher->fieldRendered('field_description');
+    $variables['card_color'] = $grapher->fieldGetOne('field_brand_color');
+    $variables['card_pattern'] = $grapher->fieldGetOne('field_brand_pattern');
+    $variables['price'] = $grapher->fieldGetOne('field_price', '', 'amount');
+    $variables['currency'] = $grapher->fieldGetOne('field_price', '', 'currency');
+    $variables['nid'] = $grapher->property('nid');
   }
 }
--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--p--vs-ticket-card.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--p--vs-ticket-card.tpl.php
@@ -4,9 +4,65 @@
  * Main template for virgin components paragraph vs_ticket_card
  *
  * Variables available:
- * TODO
+ * $title
+ * $description
+ * $card_color
+ * $card_pattern
+ * $price
+ * $currency
+ * $nid
  */
 
 ?>
 
-Ticket Card Paragraph Template
+<div class="vs-ticket-type">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="vs-ticket-type__wrapper materialShadow vs-ticket-type--gradient-<?php print $card_color;?>">
+          <?php if(!empty($title)): ?>
+          <h2 class="vs-ticket-type__title">
+            <?php print check_plain($title); ?>
+          </h2>
+          <?php endif; ?>
+          <?php if(!empty($description)): ?>
+          <div class="vs-ticket-type__text">
+            <?php print $description; ?>
+          </div>
+          <?php endif; ?>
+          <?php if(!empty($price)): ?>
+          <span class="vs-ticket-type__price"><?php print virginsport_currency($currency, $price); ?></span>
+          <?php endif; ?>
+          <button class="btn vs-btn vs-btn--sm vs-btn--transparent vs-ticket-type__cta">Select tickets</button>
+          <div class="vs-ticket-type__pass hidden-sm-down">
+            <span>Standard Pass</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!--<div class="vs-ticket-type">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="vs-ticket-type__wrapper materialShadow vs-ticket-type--gradient-blue">
+          <h2 class="vs-ticket-type__title vs-ticket-type__title--vip">
+            5K Run
+          </h2>
+          <p class="vs-ticket-type__text vs-ticket-type__text--vip">
+            Nunc laoreet ullamcorper tellus, ac lacinia metus mattis a. Praesent ut arcu ac tortor ornare mollis et ac justo. Sed eleifend eu dolor ac blandit.
+          </p>
+
+          <span class="vs-ticket-type__price">£35</span>
+          <button class="btn vs-btn vs-btn--sm vs-btn--transparent vs-ticket-type__cta">Select tickets</button>
+          <div class="vs-ticket-type__vip">
+            <span>Vip Pass</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>-->
