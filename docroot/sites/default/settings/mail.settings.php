c62ba80c6f838c8fce58deced8517618299be2d3
Graham Arrowsmith <graham.arrowsmith@acquia.com>
2016-10-23 22:17:40 +0100

    add sendgrid dev mail configuration

--- docroot/sites/default/settings/mail.settings.php
+++ docroot/sites/default/settings/mail.settings.php
@@ -12,6 +12,28 @@ if (!$is_ah_env || !in_array($ah_env, array('prod'))) {
   );
 }
 
+
+if ($ah_env && !$_ENV['AH_SITE_ENVIRONMENT'] == 'prod' ) {
+  $conf['smpt_on'] = TRUE; // Enables sending e-mails via SMTP
+  $conf['smtp_allowhtml'] = TRUE;
+  $conf['smtp_deliver'] = TRUE;
+  $conf['smtp_queue'] = FALSE; // If the e-mail should be queued before sending
+
+  // SMTP email settings
+
+  $conf['smtp_from'] = 'hello@email.virginsport.com'; // The e-mail that is sending the e-mail
+  $conf['smtp_fromname'] = 'Virgin Sport'; // The name that should appear in the e-mail
+
+  // SMTP access
+
+  $conf['smtp_host'] = 'smtp.sendgrid.net';
+  $conf['smtp_hostbackup'] = 'smtp.sendgrid.net';
+  $conf['smtp_username'] = 'virginsport-dev';
+  $conf['smtp_password'] = 'v1rg1nsp0rt';
+  $conf['smtp_port'] = 465;
+  $conf['smtp_protocol'] = 'ssl'; // options: ssl/tls
+}
+
 // SMTP Configuration
 // ------------------------------------
 
