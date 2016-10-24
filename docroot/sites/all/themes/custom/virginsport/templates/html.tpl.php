b921534127082c391464dd0cfe40fcd1362ec183
Diogo Correia <inlogic@gmail.com>
2016-10-24 12:09:14 +0100

    Set the client's google tag manager GTM id.

--- docroot/sites/all/themes/custom/virginsport/templates/html.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/html.tpl.php
@@ -60,14 +60,14 @@
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-5QBW7Q');</script>
+      })(window,document,'script','dataLayer','GTM-MDT6W8');</script>
     <!-- End Google Tag Manager -->
   </head>
 
   <body class="<?php print $classes; ?>" <?php print $attributes;?>>
     <!-- Google Tag Manager (noscript) -->
     <noscript>
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QBW7Q" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDT6W8" height="0" width="0" style="display:none;visibility:hidden"></iframe>
     </noscript>
     <!-- End Google Tag Manager (noscript) -->
 
