b1006e5d352a8c005553d70786c26945d06e6501
Diogo Correia <inlogic@gmail.com>
2016-10-29 22:48:57 +0100

    Fix whitespace in splash scripts.

--- docroot/sites/all/modules/custom/virgin_region/js/virgin_region_page_splash.js
+++ docroot/sites/all/modules/custom/virgin_region/js/virgin_region_page_splash.js
@@ -1,46 +1,47 @@
 (function ($) {
+  
+  /**
+   * Create cookie function
+   *
+   * @param name
+   *  The name of the cookie
+   * @param value
+   *  The value of the cookie
+   * @param days
+   *  The number of days to expire
+   */
+  function createCookie(name, value, days) {
+    // Convert days to timestamp
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    
+    // Global drupal cookie domain variable provided by virgin_Region
+    var cookieDomain = Drupal.settings.virgin_region.cookie_domain;
+    
+    // Set the cookie parameters
+    var cookieParts = [
+      (encodeURIComponent(name) + "=" + encodeURIComponent(value)),
+      "; expires=" + date.toGMTString(),
+      "; path=/",
+      "; domain=" + cookieDomain
+    ];
+    
+    // Save the cookie
+    document.cookie = cookieParts.join('');
+  }
+  
+  /**
+   * Executes on document ready
+   */
+  $(document).ready(function () {
 
-    /**
-     * Create cookie function
-     *
-     * @param name
-     *  The name of the cookie
-     * @param value
-     *  The value of the cookie
-     * @param days
-     *  The number of days to expire
-     */
-    function createCookie(name, value, days) {
-        // Convert days to timestamp
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    // Register a click handler to all the region selection links to
+    // ensure the user preference is set.
+    $('a[region-hostname]').click(function () {
 
-        // Global drupal cookie domain variable provided by virgin_Region
-        var cookieDomain = Drupal.settings.virgin_region.cookie_domain;
-
-        // Set the cookie parameters
-        var cookieParts = [
-            (encodeURIComponent(name) + "=" + encodeURIComponent(value)),
-            "; expires=" + date.toGMTString(),
-            "; path=/",
-            "; domain=" + cookieDomain
-        ];
-
-        // Save the cookie
-        document.cookie = cookieParts.join('');
-    }
-
-    /**
-     * Executes on document ready
-     */
-    $(document).ready(function () {
-        /**
-         * Click event for every anchor with region-hostname attribute defined
-         */
-        $('a[region-hostname]').click(function() {
-            // Save cookie valid for one year
-            createCookie('vs_region_hostname', $(this).attr('region-hostname'), 365);
-        });
+      // Store the region preference cookie to last for a year
+      createCookie('vs_region_hostname', $(this).attr('region-hostname'), 365);
     });
-
+  });
+  
 }(jQuery));
--- docroot/sites/all/modules/custom/virgin_region/js/virgin_region_page_splash_redirect.js
+++ docroot/sites/all/modules/custom/virgin_region/js/virgin_region_page_splash_redirect.js
@@ -1,41 +1,40 @@
 (function () {
 
-    /**
-     *  Get a cookie value
-     *
-     * @param name
-     *  The name of the cookie
-     * @returns string|null
-     *  The cookie value or null if not found
-     */
-    function getCookie(name) {
-        var encodedName = encodeURIComponent(name) + "=";
+  /**
+   * Get a cookie value
+   *
+   * @param name
+   *  The name of the cookie
+   * @returns string|null
+   *  The cookie value or null if not found
+   */
+  function getCookie(name) {
+    var encodedName = encodeURIComponent(name) + "=";
 
-        // Split cookie list string into an array, by default cookies are
-        // separated by a ; and white space
-        var cookieList = document.cookie.split('; ');
+    // Split cookie list string into an array, by default cookies are
+    // separated by a ; and white space
+    var cookieList = document.cookie.split('; ');
 
-        for (var i = 0; i < cookieList.length; i++) {
-            var cookie = cookieList[i];
+    for (var i = 0; i < cookieList.length; i++) {
+      var cookie = cookieList[i];
 
-            // If encoded name exists return the value portion of cookie, that comes
-            // after the encoded name
-            if (cookie.indexOf(encodedName) === 0) {
-                var cookieValue = cookie.substring(encodedName.length, cookie.length);
-                return decodeURIComponent(cookieValue);
-            }
-        }
-
-        // Default cookie value
-        return null;
+      // If encoded name exists return the value portion of cookie, that comes
+      // after the encoded name
+      if (cookie.indexOf(encodedName) === 0) {
+        var cookieValue = cookie.substring(encodedName.length, cookie.length);
+        return decodeURIComponent(cookieValue);
+      }
     }
 
-    var regionHostname = getCookie('vs_region_hostname');
+    // Default cookie value
+    return null;
+  }
 
-    if (regionHostname && location.hostname != regionHostname) {
-        var protocol = location.protocol;
+  var regionHostname = getCookie('vs_region_hostname');
 
-        location.href = protocol + '//' + regionHostname;
-    }
+  // If the region cookie is already set, immediatly redirect the user
+  if (regionHostname && location.hostname != regionHostname) {
+    location.href = location.protocol + '//' + regionHostname;
+  }
 
 }());
