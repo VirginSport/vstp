c9a82223165020d194dd948b3cefd04df5ad0b41
Luís Freitas <luis.freitas@driven.pt>
2016-11-01 15:42:59 +0000

    Add js file

---
+++ docroot/sites/all/themes/custom/virginsport/src/js/components/vs-sharer.js
@@ -0,0 +1,5 @@
+import $ from '../lib/jquery';
+
+export default () => {
+  alert("entrei");
+};
--- docroot/sites/all/themes/custom/virginsport/src/js/main.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/main.js
@@ -3,10 +3,12 @@
 import vsHeader from './components/vs-header';
 import vsRegion from './components/vs-region';
 import vsIPE from './components/vs-ipe';
+import vsSharer from './components/vs-sharer';
 
 vsHeader();
 vsRegion();
 vsIPE();
+vsSharer();
 
 // Libs
 import bootstrap from './lib/bootstrap';
