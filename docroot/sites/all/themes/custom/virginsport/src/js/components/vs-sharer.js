d44bb8a4e2e3ce0f59db1a117a354aba8b90b14a
Luís Freitas <luis.freitas@driven.pt>
2016-11-01 19:28:17 +0000

    Add javascript

--- docroot/sites/all/themes/custom/virginsport/src/js/components/vs-sharer.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/components/vs-sharer.js
@@ -1,5 +1,20 @@
 import $ from '../lib/jquery';
 
 export default () => {
-  alert("entrei");
+  share();
 };
+
+function share() {
+  $('.icon-share').click(function () {
+    if ($('.vs-share-button__icon-share--active').length > 0){
+      $('.vs-share-button__icon-share').removeClass('vs-share-button__icon-share--active');
+      $('.vs-share-button__social-links').removeClass('vs-share-button__social-links--active');
+      $('.vs-share-button__content').removeClass('vs-share-button__content--active');
+    }
+    else {
+      $('.vs-share-button__icon-share').addClass('vs-share-button__icon-share--active');
+      $('.vs-share-button__social-links').addClass('vs-share-button__social-links--active');
+      $('.vs-share-button__content').addClass('vs-share-button__content--active');
+    }
+  })
+}
