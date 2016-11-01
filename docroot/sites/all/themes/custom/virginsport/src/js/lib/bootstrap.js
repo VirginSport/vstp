5e815d08aa0d88f234aa247ab7661e9229fcaba1
GomeZ <joao.gomes@driven.pt>
2016-11-01 15:47:12 +0000

    Add tabs to drupal behaviours

--- docroot/sites/all/themes/custom/virginsport/src/js/lib/bootstrap.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/lib/bootstrap.js
@@ -10,6 +10,13 @@ export default () => {
         $el.addClass('vs-collapse--found');
         (new bootstrap.Collapse(el, { duration: $el.attr('data-duration') }))
       });
+
+      $('[data-toggle="tab"], [data-toggle="pill"]').each((idx, el) => {
+        let $el = $(el);
+        var duration = $el.attr('data-duration') && $el.getAttribute('data-duration') || false;
+
+        (new bootstrap.Tab(el, { duration: duration }))
+      });
     }
   };
 };
