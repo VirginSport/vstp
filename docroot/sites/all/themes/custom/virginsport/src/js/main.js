44f8cab246ad2b9eeb048ba610dc9e97cce2a0d9
Diogo Correia <inlogic@gmail.com>
2016-11-01 17:03:19 +0000

    Add a script to read the cookie counter and update basket value automatically.

--- docroot/sites/all/themes/custom/virginsport/src/js/main.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/main.js
@@ -1,10 +1,12 @@
 
 // Components
+import vsBasket from './components/vs-basket';
 import vsHeader from './components/vs-header';
 import vsIFrame from './components/vs-iframe';
 import vsIPE from './components/vs-ipe';
 import vsRegion from './components/vs-region';
 
+vsBasket();
 vsHeader();
 vsIFrame();
 vsIPE();
