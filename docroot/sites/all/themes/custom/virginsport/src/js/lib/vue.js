0629a2827945675e7f79ab991e6937691d65ee9e
GomeZ <joao.gomes@driven.pt>
2016-11-02 19:05:19 +0000

    Add vue validator custom email validator

--- docroot/sites/all/themes/custom/virginsport/src/js/lib/vue.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/lib/vue.js
@@ -3,4 +3,14 @@ import VueValidator from 'vue-validator';
 
 Vue.use(VueValidator);
 
+/*
+ * Check if is a valid email
+ */
+Vue.validator('email', {
+  message: 'invalid email address',
+  check: function (val) {
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
+  }
+});
+
 export default Vue;
