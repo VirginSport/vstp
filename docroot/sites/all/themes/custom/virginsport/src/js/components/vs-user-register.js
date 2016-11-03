f7871e63aa92b2ebf3ce332df16bd5e13d684172
GomeZ <joao.gomes@driven.pt>
2016-11-03 22:33:34 +0000

    Add user register js

---
+++ docroot/sites/all/themes/custom/virginsport/src/js/components/vs-user-register.js
@@ -0,0 +1,128 @@
+import $ from '../lib/jquery';
+import places from '../lib/google-places';
+import Vue from '../lib/vue';
+
+let fieldMap = {
+  'input#edit-mail': 'field_mail',
+  'input#edit-mail-confirm': 'field_confirm_mail',
+  'input#edit-pass-pass1': 'pass1',
+  'input#edit-pass-pass2': 'pass2',
+  '#edit-field-first-name input': 'field_first_name',
+  '#edit-field-last-name input': 'field_last_name',
+  '#edit-field-gender select': 'field_gender',
+  '.date-day select': 'field_date_day',
+  '.date-month select': 'field_date_month',
+  '.date-year select': 'field_date_year',
+  '#edit-field-marketing-optin input': 'field_marketing_optin',
+  '#edit-field-contact-number input': 'field_contact_number',
+  '#edit-field-address-line-1 input': 'field_address_line_1',
+  '#edit-field-address-line-2 input': 'field_address_line_2',
+  '#edit-field-address-city input': 'field_address_city',
+  '#edit-field-address-state input': 'field_address_state',
+  '#edit-field-address-postcode input': 'field_address_postcode',
+  '#edit-field-address-country select': 'field_address_country'
+};
+
+export default () => {
+  let selector = '#vs-user-register';
+
+  if (!$(selector).length) {
+    return;
+  }
+
+  new Vue({
+    el: selector,
+    compiled() {
+
+      this.bindAutocomplete();
+      this.populateLists();
+      this.setValues();
+    },
+    methods: {
+
+      bindAutocomplete() {
+        let acField = $('#google-autocomplete').get(0);
+
+        places(acField, (p) => {
+          this.profile.field_address_line_1 = `${p.long('route')} ${p.long('street_number')}`;
+          this.profile.field_address_city = p.short('locality');
+          this.profile.field_address_country = p.short('country');
+          this.profile.field_address_state = p.short('administrative_area_level_1');
+          this.profile.field_address_postcode = p.short('postal_code');
+        });
+      },
+
+      populateLists() {
+        for (let selector in fieldMap) {
+          if (selector.indexOf(' select') !== -1) {
+            let field = fieldMap[selector];
+            let $options = $(selector).find('> option').clone();
+
+            $('[name=' + field + ']').append($options);
+          }
+        }
+      },
+
+      /**
+       * Get values from drupal form into vue model
+       */
+      setValues() {
+        for (let selector in fieldMap) {
+          let field = fieldMap[selector];
+          let element = $(selector);
+          let value = element.val();
+
+          if (element.is(':checkbox')) {
+            value = element.prop('checked')
+          }
+
+          this.profile[field] = value;
+        }
+      },
+
+      /**
+       * Apply vue model values to drupal form
+       */
+      applyValues() {
+        for (let selector in fieldMap) {
+          let field = fieldMap[selector];
+          let element = $(selector);
+          let value = this.profile[field];
+          element.val(value);
+
+          if (element.is(':checkbox')) {
+            element.prop('checked', value);
+          }
+        }
+      },
+
+      submit() {
+        this.applyValues();
+        let form = $('.vs-user-login--form-drupal form').submit();
+      }
+    },
+    data: {
+      address_manual: false,
+      profile: {
+        current_pass: '',
+        pass1: '',
+        pass2: '',
+        field_mail: '',
+        field_confirm_mail: '',
+        field_first_name: '',
+        field_last_name: '',
+        field_date_day: '',
+        field_date_month: '',
+        field_date_year: '',
+        field_gender: '',
+        field_marketing_optin: '',
+        field_address_line_1: '',
+        field_address_line_2: '',
+        field_address_city: '',
+        field_address_state: '',
+        field_address_postcode: '',
+        field_address_country: [],
+      }
+    }
+  });
+};
--- docroot/sites/all/themes/custom/virginsport/src/js/main.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/main.js
@@ -7,6 +7,7 @@ import vsIPE from './components/vs-ipe';
 import vsRegion from './components/vs-region';
 import vsUserProfile from './components/vs-user-profile';
 import vsUserLogin from './components/vs-user-login';
+import vsUserRegister from './components/vs-user-register';
 
 vsBasket();
 vsHeader();
@@ -15,6 +16,7 @@ vsIPE();
 vsRegion();
 vsUserProfile();
 vsUserLogin();
+vsUserRegister();
 
 
 // Libs
