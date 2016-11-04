d5db0f6904f02f70acfacd70cc8337c512d4573f
Luís Freitas <luis.freitas@driven.pt>
2016-11-04 19:40:25 +0000

    Add the necessary script

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-hootsuite-social-feed.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-hootsuite-social-feed.tpl.php
@@ -4,9 +4,12 @@
  * Main template for virgin components fieldable panel panes vs_hootsuite_feed
  *
  * Variables available:
- * $title - The campaign title
- * $campaign_url - The campaign url
+ * @var $title
+ *  The campaign title
+ * @var $campaign_url
+ *  The campaign url
  */
 ?>
 
-<iframe width="100%" height="500px" frameBorder="0" scrolling="no" src="<?php print $campaign_url; ?>?hidesections=header,footer,navigation,&redirect=false" id="bk_iframe" onload="resizeCrossDomainIframe('bk_iframe', 'http://virginsport.hscampaigns.com');" allowfullscreen></iframe>
+<script src="//assets.hscampaigns.com/assets/js/iframe.min.js"></script>
+<iframe width="100%" frameBorder="0" scrolling="no" src="<?php print $campaign_url; ?>?hidesections=header,footer,navigation,&redirect=false" id="bk_iframe" onload="resizeCrossDomainIframe('bk_iframe', '<?php print $campaign_url; ?>');" allowfullscreen></iframe>
