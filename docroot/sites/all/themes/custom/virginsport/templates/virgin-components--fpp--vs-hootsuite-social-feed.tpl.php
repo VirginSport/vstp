3058429e16fe1139ccda31fa94c73aa83a9f5020
Luís Freitas <luis.freitas@driven.pt>
2016-11-04 19:08:47 +0000

    Add the preprocess variables

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-hootsuite-social-feed.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-hootsuite-social-feed.tpl.php
@@ -4,7 +4,9 @@
  * Main template for virgin components fieldable panel panes vs_hootsuite_feed
  *
  * Variables available:
- * TODO
+ * $title - The campaign title
+ * $campaign_url - The campaign url
  */
 ?>
-<iframe width="100%" height="500px" frameBorder="0" scrolling="yes" src="http://virginsport.hscampaigns.com?hidesections=header,footer,navigation,&redirect=false" id="bk_iframe" onload="resizeCrossDomainIframe('bk_iframe', 'http://virginsport.hscampaigns.com');" allowfullscreen></iframe>
+
+<iframe width="100%" height="500px" frameBorder="0" scrolling="no" src="<?php print $campaign_url; ?>?hidesections=header,footer,navigation,&redirect=false" id="bk_iframe" onload="resizeCrossDomainIframe('bk_iframe', 'http://virginsport.hscampaigns.com');" allowfullscreen></iframe>
