5ce386abffed3cf55e9b2e40db849e98125916e4
Diogo Correia <inlogic@gmail.com>
2016-11-08 11:27:47 +0000

    Implement base work on contact form template.

---
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--contact-form.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file virgin-components--ct--contact-form.tpl.php
+ * Template for the contact form
+ *
+ * Variables available:
+ *
+ * @var string $form
+ *  The rendered contact form
+ */
+?>
+
+<?php print $pre_form; ?>
+<?php print $form; ?>
+<?php print $post_form; ?>
