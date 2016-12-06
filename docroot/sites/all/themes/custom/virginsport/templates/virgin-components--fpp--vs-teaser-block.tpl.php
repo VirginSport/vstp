f2c7f137f71a19b57d91dfd557ba034728021e2a
Diogo Correia <inlogic@gmail.com>
2016-12-06 17:39:50 +0000

    Add brand_color field to teaser block component.

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-teaser-block.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-teaser-block.tpl.php
@@ -4,11 +4,16 @@
  * Main template for virgin components fieldable panel panes vs_teaser_block
  *
  * Variables available:
- * $paragraphs - an array of paragraphs
+ * @var stdClass[] $paragraphs
+ *  an array of paragraphs
+ * @var string $brand_color
+ *  The brand color key
+ * @var stdClass $hero_banner_image
+ *  The hero banner image atom
  */
 ?>
 
-<div class="vs-introduction__container-wrapper">
+<div class="vs-introduction__container-wrapper vs-introduction--gradient-<?php print $brand_color; ?>">
   <div class="vs-introduction__image" style="<?php print virginsport_atom_background($hero_banner_image->getEntity()); ?>"></div>
 
   <div class="vs-introduction__content">
