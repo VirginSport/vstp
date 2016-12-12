5f58677377af720b901bbc41eb476e8b08ff0466
Graham Arrowsmith <graham.arrowsmith@acquia.com>
2016-12-12 08:06:24 +0000

    per environment sendgrid settings

--- docroot/sites/default/settings/mail.settings.php
+++ docroot/sites/default/settings/mail.settings.php
@@ -12,24 +12,35 @@
 //  );
 //}
 
-
-if ($is_ah_env && !in_array($ah_env, array('prod'))) {
+if ($is_ah_env) {
   $conf['smtp_on'] = TRUE; // Enables sending e-mails via SMTP
   $conf['smtp_allowhtml'] = TRUE;
   $conf['smtp_deliver'] = TRUE;
   $conf['smtp_queue'] = FALSE; // If the e-mail should be queued before sending
 
   // SMTP email settings
-
   $conf['smtp_from'] = 'hello@email.virginsport.com'; // The e-mail that is sending the e-mail
   $conf['smtp_fromname'] = 'Virgin Sport'; // The name that should appear in the e-mail
 
   // SMTP access
-
   $conf['smtp_host'] = 'smtp.sendgrid.net';
   $conf['smtp_hostbackup'] = 'smtp.sendgrid.net';
-  $conf['smtp_username'] = 'virginsport-dev';
-  $conf['smtp_password'] = 'v1rg1nsp0rt';
   $conf['smtp_port'] = 465;
   $conf['smtp_protocol'] = 'ssl';
+
+  // Prod settings stored in secrets.settings.php
+  switch ($ah_env) {
+    case 'prelaunch':
+      $conf['smtp_username'] = 'drupal-sendgrid-prelaunch';
+      $conf['smtp_password'] = 'sendGrid1234';
+      break;
+    case 'test':
+      $conf['smtp_username'] = 'drupal-sendgrid-stg';
+      $conf['smtp_password'] = 'sendGrid1234';
+      break;
+    case 'dev':
+      $conf['smtp_username'] = 'drupal-sendgrid-dev';
+      $conf['smtp_password'] = 'sendGrid1234';
+      break;
+  }
 }
