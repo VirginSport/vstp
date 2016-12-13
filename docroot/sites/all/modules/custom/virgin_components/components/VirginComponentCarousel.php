f2f6356a598ec4141a14f6f12985754e549b6fd9
Luís Freitas <luis.freitas@driven.pt>
2016-12-13 18:50:32 +0000

    Add carousel template

--- docroot/sites/all/modules/custom/virgin_components/components/VirginComponentCarousel.php
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentCarousel.php
@@ -28,6 +28,48 @@ class VirginComponentCarousel implements VirginComponentsInterface {
    * {@inheritdoc}
    */
   public function preProcess(&$variables) {
-    // TODO
+    if (empty($variables['elements']['#fieldable_panels_pane'])) {
+      return;
+    }
+
+    $variables['fpp'] = $variables['elements']['#fieldable_panels_pane'];
+    $virgin = new VirginEntityGrapher('fieldable_panels_pane', $variables['fpp']);
+
+    $variables['title'] = $virgin->fieldGetOne('title_field');
+    $variables['heading'] = $virgin->fieldGetOne('field_component_heading');
+    $variables['carousel_style'] = $virgin->fieldGetOne('field_carousel_style');
+    $variables['thumbnail'] = $virgin->fieldGetOne('field_thumbnails');
+    $banners = array();
+
+    foreach($variables['field_banners'] as $banner) {
+      $entity = entity_load_single('paragraphs_item', $banner['value']);
+      $grapher = new VirginEntityGrapher('paragraphs_item', $entity);
+      $bundle = $grapher->property('bundle');
+
+      $picture = NULL;
+
+      if ($bundle == 'vs_custom_banner') {
+        $picture = $grapher->relation('field_hero_image');
+      }
+
+      if ($bundle == 'vs_event_festival_banner') {
+        $picture = $grapher->relation('field_festival_event_reference')->relation('field_header_image');
+      }
+
+      if ($bundle == 'vs_slide'){
+        $picture = $grapher->relation('field_slide_image');
+      }
+
+      if (empty($picture)) {
+        continue;
+      }
+
+      $banners[] = array(
+        'entity_grapher' => $grapher,
+        'picture' => $picture
+      );
+    }
+
+    $variables['banners'] = $banners;
   }
 }
--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-carousel.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-carousel.tpl.php
@@ -7,5 +7,54 @@
  * TODO
  */
 ?>
+<div class="vs-photo-gallery">
+  <div class="vs-photo-gallery-wrapper">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="vs-photo-gallery__image-wrapper">
+          <?php foreach ($variables['banners'] as $banner): ?>
+            <?php print theme('virginsport_picture', array('atom_id' => $banner['picture']->property('sid'), 'image_style' => 'virgin_original', 'image_classes' => 'vs-photo-gallery__image')); ?>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
 
-Carousel Template
+    <?php if ($thumbnail == TRUE): ?>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="vs-photo-gallery__thumbnails-wrapper">
+          <?php foreach ($variables['banners'] as $banner): ?>
+            <div class="vs-photo-gallery__thumbnail" style="<?php print virginsport_atom_background($banner['picture']->getEntity()); ?>"></div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+    <?php endif; ?>
+  </div>
+  <!-- Fullscreen view -->
+  <div class="vs-photo-gallery-wrapper--fullscreen modal fade" tabindex="-1" role="dialog" data-backdrop="static">
+
+    <button
+      type="button"
+      class="close"
+      data-dismiss="modal"
+      aria-label="Close"
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+
+    <div class="vs-photo-gallery__image-wrapper">
+      <?php foreach ($variables['banners'] as $banner): ?>
+        <?php print theme('virginsport_picture', array('atom_id' => $banner['picture']->property('sid'), 'image_style' => 'virgin_original', 'image_classes' => 'img-fluid')); ?>
+      <?php endforeach; ?>
+    </div>
+
+    <?php if ($thumbnail == TRUE): ?>
+      <div class="vs-photo-gallery__thumbnails-wrapper">
+      <?php foreach ($variables['banners'] as $banner): ?>
+        <div title="{{ photo.title }}" class="vs-photo-gallery__thumbnail" style="<?php print virginsport_atom_background($banner['picture']->getEntity()); ?>"></div>
+      <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
