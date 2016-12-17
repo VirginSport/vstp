a99c46c0d8438369d6176659912046b65adbb945
Diogo Correia <inlogic@gmail.com>
2016-12-17 14:51:09 +0000

    Fix splash redirect not working.
    Due to a non-related javascript file error, the redirect javascript mechanism was not being executed.

--- docroot/sites/all/themes/custom/virginsport/src/js/components/vs-cookie.js
+++ docroot/sites/all/themes/custom/virginsport/src/js/components/vs-cookie.js
@@ -8,6 +8,10 @@ import cookie from 'js-cookie';
 export default () => {
   Drupal.behaviors.virginsport = {
     attach: () => {
+      if (!Drupal.settings.virginsport || !Drupal.settings.virginsport.cookie_template) {
+        return;
+      }
+      
       let cookie_class = 'vs-notification--cookie';
       let cookie_key = 'cookie_accepted';
       let cookie_accepted = cookie.get(cookie_key);
