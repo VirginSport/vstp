ea519aa454a0dae2829f3ad730277ad8c483c00e
Luís Freitas <luis.freitas@driven.pt>
2016-12-18 01:33:49 +0000

    Add carousel html structure and print variables

--- docroot/sites/all/modules/custom/virgin_components/components/VirginComponentCarousel.php
+++ docroot/sites/all/modules/custom/virgin_components/components/VirginComponentCarousel.php
@@ -34,39 +34,23 @@ class VirginComponentCarousel implements VirginComponentsInterface {
 
     $variables['fpp'] = $variables['elements']['#fieldable_panels_pane'];
     $virgin = new VirginEntityGrapher('fieldable_panels_pane', $variables['fpp']);
-
     $variables['title'] = $virgin->fieldGetOne('title_field');
     $variables['heading'] = $virgin->fieldGetOne('field_component_heading');
     $variables['carousel_style'] = $virgin->fieldGetOne('field_carousel_style');
-    $variables['thumbnail'] = $virgin->fieldGetOne('field_thumbnails');
+    $variables['autoplay'] = $virgin->fieldGetOne('field_autoplay');
+    $variables['brand_color'] = $virgin->fieldGetOne('field_brand_color');
+    $x = 0;
     $banners = array();
 
     foreach($variables['field_banners'] as $banner) {
       $entity = entity_load_single('paragraphs_item', $banner['value']);
       $grapher = new VirginEntityGrapher('paragraphs_item', $entity);
+
       $bundle = $grapher->property('bundle');
-
-      $picture = NULL;
-
-      if ($bundle == 'vs_custom_banner') {
-        $picture = $grapher->relation('field_hero_image');
-      }
-
-      if ($bundle == 'vs_event_festival_banner') {
-        $picture = $grapher->relation('field_festival_event_reference')->relation('field_header_image');
-      }
-
-      if ($bundle == 'vs_slide'){
-        $picture = $grapher->relation('field_slide_image');
-      }
-
-      if (empty($picture)) {
-        continue;
-      }
+      $variables['bundle'] = $bundle;
 
       $banners[] = array(
-        'entity_grapher' => $grapher,
-        'picture' => $picture
+        'entity_grapher' => $entity,
       );
     }
 
--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-carousel.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-carousel.tpl.php
@@ -6,55 +6,111 @@
  * Variables available:
  * TODO
  */
+
+$width = ($carousel_style == 'full_width') ? 'vs-carousel-wrapper' : 'vs-carousel-wrapper--contained';
+$autoplay = ($variables['autoplay'] == TRUE) ? 'autoplay' : '';
+
 ?>
-<div class="vs-photo-gallery">
-  <div class="vs-photo-gallery-wrapper">
+
+<!--Carousel-->
+<div class="vs-carousel">
+
+  <div class="<?php print $width; ?>">
+
     <div class="row">
-      <div class="col-xs-12">
-        <div class="vs-photo-gallery__image-wrapper">
-          <?php foreach ($variables['banners'] as $banner): ?>
-            <?php print theme('virginsport_picture', array('atom_id' => $banner['picture']->property('sid'), 'image_style' => 'virgin_original', 'image_classes' => 'vs-photo-gallery__image')); ?>
-          <?php endforeach; ?>
-        </div>
+      <div class="col-xs-12 vs-carousel__item-wrapper <?php print $autoplay; ?>">
+        <?php foreach ($variables['banners'] as $banner): ?>
+
+          <?php $slide_item = new VirginEntityGrapher('paragraphs_item', $banner['entity_grapher']); ?>
+
+          <!-- vs_custom_banner -->
+
+          <?php if($slide_item->property('bundle') == 'vs_custom_banner') : ?>
+
+            <div class="vs-carousel__item">
+              <div class="vs-promo-banner vs-promo-banner--color-<?php print $variables['brand_color']; ?>">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="vs-promo-banner-block-wrapper">
+                      <div class="vs-promo-banner__background" style="<?php print virginsport_atom_background($slide_item->relation('field_hero_image')->getEntity()); ?>"></div>
+                      <div class="col-xs-12 vs-promo-banner__content-wrapper">
+                        <h1 class="vs-promo-banner__title"><?php print $slide_item->fieldGetOne('title_field'); ?></h1>
+                        <div class="vs-promo-banner__content">
+                          <?php print $slide_item->fieldGetOne('field_description_short'); ?>
+                        </div>
+                        <div class="vs-promo-banner__ctaButton-wrapper">
+                            <?php print
+                              theme('virginsport_cta_links',
+                                array(
+                                  'links' => $slide_item->fieldGetAll('field_cta_links'),
+                                  'classes' => 'vs-btn--min-sm vs-promo-banner__ctaButton'
+                                )
+                              );
+                            ?>
+                        </div>
+                      </div>
+                      <div class="hidden-md-down vs-promo-banner__caption"><?php print $slide_item->fieldGetOne('field_caption'); ?></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          <?php endif; ?>
+
+          <!-- vs_event_festival_banner -->
+
+          <?php if($slide_item->property('bundle') == 'vs_event_festival_banner') : ?>
+
+            <?php $event = $slide_item->relation('field_festival_event_reference'); ?>
+
+            <div class="vs-carousel__item">
+              <div class="vs-promo-banner vs-promo-banner--color-<?php print $variables['brand_color']; ?>">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="vs-promo-banner-block-wrapper">
+                      <div class="vs-promo-banner__background" style="<?php print virginsport_atom_background($event->relation('field_header_image')->getEntity()); ?>"></div>
+                      <div class="col-xs-12 vs-promo-banner__content-wrapper">
+                        <h1 class="vs-promo-banner__title"><?php print $event->fieldGetOne('title_field'); ?></h1>
+                        <div class="vs-promo-banner__content">
+                          <?php print $event->fieldGetOne('field_description'); ?>
+                        </div>
+                        <div class="vs-promo-banner__ctaButton-wrapper">
+                          <?php print
+                            theme('virginsport_cta_links',
+                              array(
+                                'links' => $slide_item->fieldGetAll('field_cta_links'),
+                                'classes' => 'vs-btn--min-sm vs-promo-banner__ctaButton'
+                              )
+                            );
+                          ?>
+                        </div>
+                      </div>
+                      <div class="hidden-md-down vs-promo-banner__caption"><?php print $slide_item->fieldGetOne('field_caption'); ?></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          <?php endif; ?>
+
+          <!-- vs_slide -->
+
+          <?php if($slide_item->property('bundle') == 'vs_slide') : ?>
+
+            <div class="vs-carousel__item">
+              <div class="vs-carousel__image vs-carousel__image--color-<?php print $variables['brand_color']; ?>" style="<?php print virginsport_atom_background($slide_item->relation('field_slide_image')->getEntity()); ?>">
+                <div class="vs-carousel__image-description"><?php print $slide_item->fieldGetOne('field_description')?></div>
+              </div>
+            </div>
+
+          <?php endif; ?>
+
+        <?php endforeach; ?>
       </div>
     </div>
 
-    <?php if ($thumbnail == TRUE): ?>
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="vs-photo-gallery__thumbnails-wrapper">
-          <?php foreach ($variables['banners'] as $banner): ?>
-            <div class="vs-photo-gallery__thumbnail" style="<?php print virginsport_atom_background($banner['picture']->getEntity()); ?>"></div>
-          <?php endforeach; ?>
-        </div>
-      </div>
-    </div>
-    <?php endif; ?>
-  </div>
-  <!-- Fullscreen view -->
-  <div class="vs-photo-gallery-wrapper--fullscreen modal fade" tabindex="-1" role="dialog" data-backdrop="static">
-
-    <button
-      type="button"
-      class="close"
-      data-dismiss="modal"
-      aria-label="Close"
-    >
-      <span aria-hidden="true">&times;</span>
-    </button>
-
-    <div class="vs-photo-gallery__image-wrapper">
-      <?php foreach ($variables['banners'] as $banner): ?>
-        <?php print theme('virginsport_picture', array('atom_id' => $banner['picture']->property('sid'), 'image_style' => 'virgin_original', 'image_classes' => 'img-fluid')); ?>
-      <?php endforeach; ?>
-    </div>
-
-    <?php if ($thumbnail == TRUE): ?>
-      <div class="vs-photo-gallery__thumbnails-wrapper">
-      <?php foreach ($variables['banners'] as $banner): ?>
-        <div title="{{ photo.title }}" class="vs-photo-gallery__thumbnail" style="<?php print virginsport_atom_background($banner['picture']->getEntity()); ?>"></div>
-      <?php endforeach; ?>
-      </div>
-    <?php endif; ?>
   </div>
 </div>
+
