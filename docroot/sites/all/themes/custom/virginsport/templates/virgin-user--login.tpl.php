2f93333b6bbf8fdbe44e4f099231cb9fa2fcd0fc
GomeZ <joao.gomes@driven.pt>
2017-01-18 15:43:47 +0000

    Change field type to email and add attributes to not capitalize field

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-user--login.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-user--login.tpl.php
@@ -72,7 +72,7 @@ $query = drupal_get_query_parameters();
                         <div class="col-xs-12">
                           <div class="vs-user-form__wrapper">
                             <div class="field-name vs-form-group" :class="{ 'vs-has-error': ($vs_user_login_validator.name.dirty || submitted) && !$vs_user_login_validator.name.valid }">
-                              <input id="name" class="form-control" type="text" v-model="profile.name" name="name" v-validate:name="['required']">
+                              <input autocorrect="off" autocapitalize="none" id="name" class="form-control" type="email" v-model="profile.name" name="name" v-validate:name="['required']">
                               <label for="name" class="vs-focus-label"><?php print t('Email'); ?>*</label>
                               <div class="vs-error-label" v-if="($vs_user_login_validator.name.dirty || submitted) && $vs_user_login_validator.name.required"><?php print t('Name is required'); ?></div>
                             </div>
