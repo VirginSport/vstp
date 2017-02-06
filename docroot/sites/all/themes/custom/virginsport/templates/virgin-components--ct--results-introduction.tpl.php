945d02d5b3ab29442b41bdfb2d5821f92aeac0af
Nuno Neves <nuno.neves@driven.pt>
2017-02-06 18:17:52 +0000

    Add results-introduction component.

---
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--results-introduction.tpl.php
@@ -0,0 +1,19 @@
+<div class="vs-results-introduction vs-results-introduction--color-brown">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-lg-10 offset-lg-1 col-xl-11 offset-xl-0">
+        <div class="vs-results-introduction__image" style="background-image: url('/sites/all/themes/custom/virginsport/src/img/card-untimed-event-example2.png'); "></div>
+        <div class="vs-results-introductions__wave"></div>
+      </div>
+      <div class="col-xs-12 col-lg-6 offset-lg-6 col-xl-7 offset-xl-5">
+        <div class="vs-results-introduction__content">
+          <h4 class="vs-results-introduction__title">Results</h4>
+          <p class="vs-results-introduction__description">
+            Praesent et magna sit amet libero semper facilisis vel ac enim. Aliquam consectetur tellus ipsum, vel aliquet nibh elementum vitae. met libero semper facilisis vel ac enim. Ali ibh elementum vitae.
+          </p>
+          <a href="#" class="btn vs-btn vs-btn--outline-black vs-results-introduction__button">Festival page</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
