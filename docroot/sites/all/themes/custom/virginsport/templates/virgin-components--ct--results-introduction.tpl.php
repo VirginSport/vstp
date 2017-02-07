6dfe0d7d8ce279999fffdf77b9be98b4fefaae2b
Nuno Neves <nuno.neves@driven.pt>
2017-02-07 16:58:34 +0000

    Add component test integration.

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--results-introduction.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--results-introduction.tpl.php
@@ -1,17 +1,35 @@
-<div class="vs-results-introduction vs-results-introduction--color-brown">
+<?php
+
+/**
+ * @file
+ * Template file for results introduction component.
+ *
+ * @var string $title
+ * @var string $description
+ * @var string $link_label
+ * @var string $link_url
+ * @var string $link_color
+ * @var string $brand_color
+ * @var string $image_atom
+ */
+
+?>
+
+<div class="vs-results-introduction vs-results-introduction--color-<?php print $brand_color; ?>">
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-lg-10 offset-lg-1 col-xl-11 offset-xl-0">
-        <div class="vs-results-introduction__image" style="background-image: url('/sites/all/themes/custom/virginsport/src/img/card-untimed-event-example2.png'); "></div>
+        <div class="vs-results-introduction__image" style="<?php print virginsport_atom_background($image_atom); ?>"></div>
         <div class="vs-results-introductions__wave"></div>
       </div>
+      
       <div class="col-xs-12 col-lg-6 offset-lg-6 col-xl-7 offset-xl-5">
         <div class="vs-results-introduction__content">
-          <h4 class="vs-results-introduction__title">Results</h4>
-          <p class="vs-results-introduction__description">
-            Praesent et magna sit amet libero semper facilisis vel ac enim. Aliquam consectetur tellus ipsum, vel aliquet nibh elementum vitae. met libero semper facilisis vel ac enim. Ali ibh elementum vitae.
-          </p>
-          <a href="#" class="btn vs-btn vs-btn--outline-black vs-results-introduction__button">Festival page</a>
+          <h4 class="vs-results-introduction__title"><?php print check_plain($title); ?></h4>
+          <p class="vs-results-introduction__description"><?php print check_plain($description); ?></p>
+          <a href="<?php print $link_url; ?>" class="btn vs-btn vs-btn--gradient-<?php print $link_color; ?> vs-results-introduction__button">
+            <?php print check_plain($link_label); ?>
+          </a>
         </div>
       </div>
     </div>
