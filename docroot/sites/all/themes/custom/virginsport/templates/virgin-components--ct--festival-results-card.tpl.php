f8211cf5e05bcf6c5c321445403359c664fecc18
Nuno Neves <nuno.neves@driven.pt>
2017-02-08 11:29:16 +0000

    Add component test integration.

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--festival-results-card.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--festival-results-card.tpl.php
@@ -1,11 +1,30 @@
-<div class="vs-festival-results-card">
+
+<?php
+
+/**
+ * @file
+ * Template file for festival results component.
+ *
+ * @var string $festival_name
+ * @var string $festival_date
+ * @var string $race_name
+ * @var string $race_start
+ * @var string $race_end
+ * @var string $link_label
+ * @var string $link_url
+ * @var string $brand_color
+ */
+
+?>
+
+<div class="vs-festival-results-card vs-festival-results-card--color-<?php print $brand_color; ?>">
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
         <div class="vs-festival-results-card__image-wrapper">
           <div class="row">
             <div class="col-xs-12 col-md-6">
-              <div class="vs-festival-results-card__image" style="background-image: url('/sites/all/themes/custom/virginsport/src/img/card-untimed-event-example2.png'); ">
+              <div class="vs-festival-results-card__image" style="<?php print virginsport_atom_background($image_atom); ?>">
               </div>
             </div>
             <div class="col-xs-12 col-md-6">
@@ -18,8 +37,8 @@
                   </div>
                 </div>
                 <div class="vs-festival-results-card__name-date-valign">
-                  <h4 class="vs-festival-results-card__title">Hackney Festival</h4>
-                  <div class="vs-festival-results-card__date">03-04 Aug 16</div>
+                  <h4 class="vs-festival-results-card__title"><?php print check_plain($festival_name); ?></h4>
+                  <div class="vs-festival-results-card__date"><?php print check_plain($festival_date); ?></div>
                 </div>
               </div>
             </div>
@@ -33,17 +52,17 @@
           <div class="row">
             <div class="col-xs-12 col-md-8">
               <div class="vs-festival-results-card__info-block">
-                <h5 class="vs-festival-results-card__event-name">5K Run</h5>
-                <div class="vs-festival-results-card__event-date">Saturday, 03 Aug 16</div>
+                <h5 class="vs-festival-results-card__event-name"><?php print check_plain($race_name); ?></h5>
+                <div class="vs-festival-results-card__event-date"><?php print check_plain($race_start); ?></div>
               </div>
               <div class="vs-festival-results-card__info-block">
-                <h5 class="vs-festival-results-card__event-name">xxxxx</h5>
-                <div class="vs-festival-results-card__event-date">Sunday, 04 Aug 16</div>
+                <h5 class="vs-festival-results-card__event-name"><?php print check_plain($race_name); ?></h5>
+                <div class="vs-festival-results-card__event-date"><?php print check_plain($race_end); ?></div>
               </div>
             </div>
             <div class="col-xs-12 col-md-4">
               <div class="vs-festival-results-card__button-wrapper text-md-right">
-                <a href="#" class="btn vs-btn vs-btn--outline-black vs-festival-results-card__button">View results</a>
+                <a href="<?php print $link_url ?>" class="btn vs-btn vs-btn--outline-black vs-festival-results-card__button"><?php print check_plain($link_label); ?></a>
               </div>
             </div>
           </div>
