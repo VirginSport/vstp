1f7fd7b365a432f1ecdf6c0b70df63c960657cd1
GomeZ <joao.gomes@driven.pt>
2017-02-27 15:38:55 +0000

    Add location to vs_share_buttons usages

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-user--ticket.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-user--ticket.tpl.php
@@ -220,7 +220,7 @@ $event_days_left = virginsport_days_left($event_start_date);
               <?php endif; ?>
             <?php endif; ?>
             <div class="vs-ticket-card__social-wrapper">
-              <?php print theme('virginsport_share_buttons', array('subject' => $event_grapher->fieldGetOne('title_field'), 'url' => $GLOBALS['base_url'].'/node/'.$event_grapher->property('nid'))); ?>
+              <?php print theme('virginsport_share_buttons', array('location' => 'User Ticket', 'subject' => $event_grapher->fieldGetOne('title_field'), 'url' => $GLOBALS['base_url'].'/node/'.$event_grapher->property('nid'))); ?>
             </div>
           </div>
         </div>
