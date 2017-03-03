20fd4bfefaaf67b13b9d427261c4ae7e4c0273b8
Nuno Neves <nuno.neves@driven.pt>
2017-03-03 14:18:38 +0000

    Remove background image php call.

--- docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--results-overview.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--ct--results-overview.tpl.php
@@ -15,14 +15,14 @@
  */
 ?>
 
-<div class="vs-results-overview-banner vs-results-overview-banner--color-<?php print $brand_color; ?>" style="<?php print virginsport_atom_background($image_atom->getEntity()); ?>">
+<div class="vs-results-overview-banner vs-results-overview-banner--color-<?php print $brand_color; ?>">
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
         <?php print $race_name; ?>
         <?php print $race_date; ?>
         <?php print $race_description; ?>
-        <a href="<?php print $link_url; ?>"><?php print $link_label; ?></a>
+        <a href="<?php print $link_url; ?>" class="btn vs-btn vs-btn--outline-black vs-results-overview-banner__button"><?php print $link_label; ?></a>
       </div>
     </div>
   </div>
