cef2c169dcbcc674cf79e5236f04c8e790454955
Pedro Dias <pedro.dias@driven.pt>
2017-08-10 17:00:35 +0100

    The static that allows us to know if the attendly component is rendered is now a boolean

--- docroot/sites/all/themes/custom/virginsport/template.php
+++ docroot/sites/all/themes/custom/virginsport/template.php
@@ -164,8 +164,7 @@ function virginsport_preprocess_page(&$vars) {
   $vars['basket_url'] = sprintf('%s/e/checkout', $attendly_url);
   $vars['basket_cookie'] = empty($attendly_env) ? VIRGIN_USER_ATTENDLY_ITEMS_COOKIE : VIRGIN_USER_ATTENDLY_ITEMS_COOKIE . '-' . $attendly_env;
 
-  $show_checkout_bar = drupal_static(VIRGIN_ATTENDLY_IFRAME_RENDERED);
-  $vars['show_checkout_bar'] = isset($show_checkout_bar);
+  $vars['show_checkout_bar'] = drupal_static(VIRGIN_ATTENDLY_IFRAME_RENDERED);
 
   // Setup the social networks
   $vars['social_networks'] = array();
