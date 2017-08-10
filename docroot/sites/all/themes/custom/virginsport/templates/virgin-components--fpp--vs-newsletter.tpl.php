4f4f0ddb7a99db67121ccc7d1c4d41cf4c346154
Nuno Neves <nuno.neves@driven.pt>
2017-08-10 18:18:19 +0100

    Add newsletter component partial css and markup.

---
+++ docroot/sites/all/themes/custom/virginsport/templates/virgin-components--fpp--vs-newsletter.tpl.php
@@ -0,0 +1,36 @@
+
+<div class="vs-newsletter">
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-xs-12 col-md-6 hidden-sm-down">
+        <div class="vs-newsletter__image">
+          image and description
+        </div>
+      </div>
+      
+      <div class="col-xs-12 col-md-6">
+        <div class="vs-newsletter__content">
+            <h3 class="vs-newslettter__title">Be the first to know</h3>
+            <div class="vs-newslettter__text">Stay in the loop on events, news and more by signing up to our mailing list.</div>
+            <div class="vs-newslettter__form">
+              <div class="vs-form-group">
+                <input class="form-control vs-newsletter__input" id="newsletter-email" type="text" name="newsletter-email">
+                <label class="vs-focus-label" for="newsletter"><?php print t('Email Address'); ?></label>
+                <div class="vs-newsletter__button-wrapper text-xs-center text-md-left">
+                  <button class="btn vs-btn vs-btn--min-sm vs-newsletter__button"><?php print t('Submit'); ?></button>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+  
+      <div class="col-xs-12 col-md-6 hidden-md-up">
+        <div class="vs-newsletter__image">
+          image and description
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
