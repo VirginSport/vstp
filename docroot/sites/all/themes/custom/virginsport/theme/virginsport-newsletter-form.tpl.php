e024db83295fc510628e796638dd1ce1b1f02322
Nuno Neves <nuno.neves@driven.pt>
2017-08-16 18:07:51 +0100

    Fix footer newsletter css.

--- docroot/sites/all/themes/custom/virginsport/theme/virginsport-newsletter-form.tpl.php
+++ docroot/sites/all/themes/custom/virginsport/theme/virginsport-newsletter-form.tpl.php
@@ -25,7 +25,7 @@
             <p v-if="form.error" class="vs-newsletter__error-label"><?php print t('There was an error submitting your request.')?></p>
           </div>
           <?php if ($inline_button): ?>
-          <button v-if="!form.submitted" :disabled="waitingSubmit" v-on:click="submit" class="btn vs-btn vs-newsletter-footer__btn"><?php print t('Submit'); ?></button>
+          <button v-if="!form.submitted" :disabled="waitingSubmit" v-on:click="submit" class="btn vs-btn vs-newsletter__btn-inline"><?php print t('Submit'); ?></button>
           <button v-else class="btn vs-btn vs-newsletter-footer__btn" disabled><?php print t('Sent'); ?></button>
           <?php else: ?>
           <div class="vs-newsletter__button-wrapper text-xs-center text-md-left">
