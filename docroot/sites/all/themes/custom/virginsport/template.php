66ffffe9c12ddaabd298c19d8d84c39d4bc6902d
Pedro Dias <pedro.dias@driven.pt>
2017-08-29 10:43:33 +0100

    Show cookies alert based in what the user choosed

--- docroot/sites/all/themes/custom/virginsport/template.php
+++ docroot/sites/all/themes/custom/virginsport/template.php
@@ -214,9 +214,11 @@ function virginsport_preprocess_page(&$vars) {
   $vars['default_email'] = $user->uid > 0 ? $user->mail : '';
 
   // Make cookie template available in javascript
-  $message = t('We use cookies. We eat them too, but only after a run. By using this website, you agree to our use of cookies. Check out our privacy policy to learn more.');
-  $cookie_template = theme('virginsport_notification', array('message' => $message));
-  drupal_add_js(array('virginsport' => array('cookie_template' => $cookie_template)), array('type' => 'setting'));
+  if (virgin_region_current_region_show_cookie()) {
+    $message = t('We use cookies. We eat them too, but only after a run. By using this website, you agree to our use of cookies. Check out our privacy policy to learn more.');
+    $cookie_template = theme('virginsport_notification', array('message' => $message));
+    drupal_add_js(array('virginsport' => array('cookie_template' => $cookie_template)), array('type' => 'setting'));
+  }
 }
 
 /**
